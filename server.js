902f0259e224008c1ef9697dba1024096997d4d6
Angelo Bovino <angelo.bovino@outlook.com>
2017-05-25 01:04:36 -0400

    Initialized passport, required passport config, and added some comments

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const session       = require('express-session');
 const exhbs         = require('express-handlebars');
 const sequelize     = require('sequelize');
 const passport      = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const path          = require('path');
-const routes    = require('./routes/routes');
+const routes    = require('./routes/index');
 
 const PORT = 3000; // Add env variable for port later
 const app = express();
@@ -18,8 +18,17 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Initiate passport.js and session storage
+app.use(session({ secret: 'notthatsecret' }));
+app.use(passport.initialize());
+app.use(passport.session());
+// Require the passport configuration from ./config
+require('./config/passport')(passport);
+
 app.use('/', routes);
 
 app.listen(PORT, () => {
 	console.log('Server listening on PORT 3000');
 });
+
+module.exports = app;
