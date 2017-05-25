9523ace273654c2a2b54dcdd56f50828ee3b4000
Angelo Bovino <angelo.bovino@outlook.com>
2017-05-25 01:02:42 -0400

    Removed test controllers and added custom middleware

--- routes/controllers/controller.js
+++ routes/controllers/controller.js
@@ -7,40 +7,29 @@ module.exports = {
     res.render('login');
   },
 
-  handleLogin: (req, res) => {
-    console.log(req.body.email);
-    var userData = {
-      email: req.body.email,
-      password: req.body.password
-    };
-    res.json(userData);
-  },
-
   signup: (req, res) => {
     res.render('signup');
   },
 
-  handleSignUp: (req, res) => {
-    var user = req.body;
-    console.log('First name: ' + req.body.firstName);
-    console.log('Last name: ' + req.body.lastName);
-    console.log('Email: ' + req.body.email);
-    console.log('Username: ' + req.body.username);
-    console.log('DOB: ' + req.body.dob);
-    console.log('City: ' + req.body.city);
-    console.log('State: ' + req.body.state);
-    console.log('Password: ' + req.body.password);
-    var userData = {
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      username: user.username,
-      dob: user.dob,
-      city: user.city,
-      state: user.state,
-      password: user.password
-    };
+  // Custom middleware
+  middleware: {
+    // Checks to see if 'user' key exists on the req object
+    // Pass in as second argument after route
+    isAuthenticated(req, res, next) {
+      if (!req.user) {
+        res.redirect('/auth/fail')
+      }
+      // Continue to the next part of the route handler
+      return next();
+    },
+    // Makes sure the user enters the same password in the 'confirm password' input
+    confirmPassword(req, res, next) {
+      // If both passwords match continue to next part in the route handler
+      if (req.body.password === req.body.confirmPassword) {
+        return next();
+      }
+      res.json("Passwords don't match");
+    }
 
-    res.json(userData);
   }
 }
