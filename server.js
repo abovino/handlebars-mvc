b33518241344bb719966afa0286e256ea5278dfa
Angelo Bovino <angelo.bovino@outlook.com>
2017-05-27 17:50:16 -0400

    Added connect-flash and cookie-parser

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express       = require('express');
 const bodyParser    = require('body-parser');
 const session       = require('express-session');
+const cookieParser  = require('cookie-parser');
 const exhbs         = require('express-handlebars');
+const flash         = require('connect-flash');
 const sequelize     = require('sequelize');
 const passport      = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const path          = require('path');
-const routes    = require('./routes/index');
+const routes        = require('./routes/index');
 
 const PORT = 3000; // Add env variable for port later
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser('cookies'));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }));
@@ -22,6 +25,7 @@ app.set('view engine', 'handlebars');
 app.use(session({ secret: 'notthatsecret' }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 // Require the passport configuration from ./config
 require('./config/passport')(passport);
 
